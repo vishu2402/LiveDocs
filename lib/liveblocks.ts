10d1d4f67dc3d3c44577d8445cc8d20d1ff28fbb
Vishal Kumar <81041840+vishu2402@users.noreply.github.com>
2024-07-24 15:09:38 +0530

    first commit

---
+++ lib/liveblocks.ts
@@ -0,0 +1,5 @@
+import { Liveblocks } from "@liveblocks/node";
+
+export const liveblocks = new Liveblocks({
+  secret: "sk_prod_RkLvm9xWA_nswLA0ayabEBqOrrbF12Qbv5nLhMRgVl_8KNauc6cxnI4hSCRwYN55",
+});
